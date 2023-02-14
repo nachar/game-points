0ece31e0c406a0cb51a7b77cce32f2f4501425bf
hassenNachar <hnachar@gmail.com>
2023-02-14 11:35:57 +0100

    feat: test

--- src/redux/gamePointsReducer.test.tsx
+++ src/redux/gamePointsReducer.test.tsx
@@ -1,33 +1,34 @@
 import reducer, { addItem, resetGame } from './gamePointsReducer'
+describe('Store', () => {
+  test('Adding item', () => {
+    const currentItem = 'C'
+    const result = reducer(undefined, addItem(currentItem))
 
-test('Adding item', () => {
-  const currentItem = 'C'
-  const result = reducer(undefined, addItem(currentItem))
+    const selectedItem = result.scoreboard.find(item => item.name === currentItem)
 
-  const selectedItem = result.scoreboard.find(item => item.name === currentItem)
+    expect(selectedItem?.total).toBe(selectedItem?.unitPoints)
+  })
 
-  expect(selectedItem?.total).toBe(selectedItem?.unitPoints)
-})
+  test('adding items with bonuses', () => {
+    const currentItem = 'A'
+    let result = reducer(undefined, addItem(currentItem))
+    result = reducer(result, addItem(currentItem))
+    result = reducer(result, addItem(currentItem))
+    const selectedItem = result.scoreboard.find(item => item.name === currentItem)
 
-test('adding items with bonuses', () => {
-  const currentItem = 'A'
-  let result = reducer(undefined, addItem(currentItem))
-  result = reducer(result, addItem(currentItem))
-  result = reducer(result, addItem(currentItem))
-  const selectedItem = result.scoreboard.find(item => item.name === currentItem)
+    expect(selectedItem?.total).toBe(0)
+    expect(selectedItem?.bonus?.total).toBe(selectedItem?.bonus?.points)
+  })
 
-  expect(selectedItem?.total).toBe(0)
-  expect(selectedItem?.bonus?.total).toBe(selectedItem?.bonus?.points)
-})
+  test('reset application', () => {
+    const currentItem = 'B'
+    let result = reducer(undefined, addItem(currentItem))
+    let selectedItem = result.scoreboard.find(item => item.name === currentItem)
 
-test('reset application', () => {
-  const currentItem = 'B'
-  let result = reducer(undefined, addItem(currentItem))
-  let selectedItem = result.scoreboard.find(item => item.name === currentItem)
+    expect(selectedItem?.total).toBe(selectedItem?.unitPoints)
 
-  expect(selectedItem?.total).toBe(selectedItem?.unitPoints)
-
-  result = reducer(result, resetGame())
-  selectedItem = result.scoreboard.find(item => item.name === currentItem)
-  expect(selectedItem?.total).toBe(0)
+    result = reducer(result, resetGame())
+    selectedItem = result.scoreboard.find(item => item.name === currentItem)
+    expect(selectedItem?.total).toBe(0)
+  })
 })
